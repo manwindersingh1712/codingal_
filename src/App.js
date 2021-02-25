6ff0f19d3a849d6cf712b318a31f27ff3b042b8c
Manwinder Singh <manwindersingh1712@gmail.com>
2021-02-25 11:41:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/navbar";
+import NavbarRes from "./Components/navbar_mb";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      min: 0,
+      sec: 10,
+    };
+  }
+  setTime = ({ min, sec }) => {
+    this.setState({ min: min, sec: sec });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar time={this.state} setTime={this.setTime} />
+        <NavbarRes time={this.state} setTime={this.setTime} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/timer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../utils/css/timer.css";
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      min: this.props.time.min,
+      sec: this.props.time.sec,
+      interval: "",
+    };
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+    this.props.setTime(this.state);
+  }
+  componentDidMount() {
+    const interval = setInterval(() => {
+      if (this.state.sec === 0 && this.state.min === 0) {
+        clearInterval(interval);
+        return;
+      }
+      if (this.state.sec === 0 && this.state.min !== 0) {
+        this.setState({ min: this.state.min - 1, sec: 59 });
+      } else {
+        this.setState({ sec: this.state.sec - 1 });
+      }
+    }, 1000);
+    this.setState({ interval: interval });
+    console.log("ended");
+  }
+  render() {
+    return (
+      <div className="timer">
+        {this.state.min < 10 ? "0" + this.state.min : this.state.min}:
+        {this.state.sec < 10 ? "0" + this.state.sec : this.state.sec}
+      </div>
+    );
+  }
+}
+
+export default Timer;
